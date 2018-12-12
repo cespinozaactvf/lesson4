f2cc155a86e95018ce4f4d579bed1f4e7139d8e5
cespinozaactvf <44033422+cespinozaactvf@users.noreply.github.com>
2018-12-11 23:00:33 -0500

    Add files via upload

---
+++ Lesson 4/Problem 1/problem1.py
@@ -0,0 +1,7 @@
+score = 5
+
+result = (score / 4)* 100
+
+result = round(result)
+
+print(result)
---
+++ Lesson 4/Problem 3/problem3.py
@@ -0,0 +1,8 @@
+english = 94
+math = 82
+hisotry = 81
+science = 95
+
+average = (english + math + history + science)
+
+print(average)
